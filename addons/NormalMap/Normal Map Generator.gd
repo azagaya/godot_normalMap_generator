d72cb96b541453a6f5a5a57eaa8964c14abaf407
TwistedTwigleg <beard.noah@gmail.com>
2019-07-14 16:39:08 -0400

    Changed UI layout to allow for resizing in plugin window. It is not perfect, but now it reacts better to different window sizes. Added code to move the Light2D node and change its color using the _input function. Other minor changes here and there, comments for all the code I added

--- addons/NormalMap/Normal Map Generator.gd
+++ addons/NormalMap/Normal Map Generator.gd
@@ -4,18 +4,19 @@ extends Control
 var texture_rect
 var viewport
 var current_path = "./generated.png"
-var track_mouse = false
+var light2d_node = null
+var viewport_container_node = null
+
 
 func _ready():
-	texture_rect = $VBoxContainer/ViewportContainer/Viewport/TextureRect
-	viewport = $VBoxContainer/ViewportContainer/Viewport
+	texture_rect = $GUI/ViewportContainer/Viewport/TextureRect
+	viewport = $GUI/ViewportContainer/Viewport
 	viewport.size = texture_rect.texture.get_size()
+	
+	light2d_node = $GUI/ViewportContainer/Viewport/TextureRect/Light2D
+	viewport_container_node = $GUI/ViewportContainer
+	$GUI/HBoxContainer_ColorPicker/ColorPickerButton.color = light2d_node.color
 
-func _process(delta):
-	if (track_mouse):
-		if Input.is_action_pressed("click"):
-			print("click")
-			$VBoxContainer/ViewportContainer/Viewport/TextureRect/Light2D.global_position = get_global_mouse_position()
 
 func _on_Normal_toggled(button_pressed):
 	texture_rect.material.set_shader_param("normal_preview",button_pressed)
@@ -57,8 +58,12 @@ func _on_Button_pressed():
 	img.flip_y()
 	print(img.save_png(current_path))
 
+
 func _on_TextureButton_pressed():
-	$FileDialog.popup()
+	# Make the file dialog half the size of the Godot editor and make it popup in the center.
+	$FileDialog.rect_size = get_tree().root.size / 2;
+	$FileDialog.popup_centered();
+
 
 func _on_FileDialog_file_selected(path):
 	var img = Image.new()
@@ -68,9 +73,35 @@ func _on_FileDialog_file_selected(path):
 		texture_rect.texture = itex
 		var aux = path.rsplit(".",true,1)
 		current_path = aux[0]+"_n."+aux[1]
+		
+		# Change viewport size to match the new image size
+		$GUI/ViewportContainer/Viewport.size = img.get_size();
 
-func _on_TextureRect_mouse_entered():
-	track_mouse = true;
 
-func _on_TextureRect_mouse_exited():
-	track_mouse = false;
+# Used for tracking the mouse and other input events.
+# Currently this is only used to move the Light2D node.
+func _input(event):
+	if (event is InputEventMouseButton):
+		
+		var mouse_pos = get_tree().root.get_mouse_position();
+		
+		# Check if the mouse is within the bounds of the Viewport container
+		# NOTE: this is perhaps not the most performance friendly way of checking, may need to be replaced later.
+		if Rect2(viewport_container_node.rect_global_position, viewport_container_node.rect_size).has_point(get_tree().root.get_mouse_position()):
+			if event.pressed == true and event.button_index == BUTTON_LEFT:
+				
+				# First, get the position of the mouse within the rectangle of the ViewportContainer
+				var relative_mouse_pos = mouse_pos - rect_global_position
+				
+				# Get the position relative to the Viewport.
+				# First, convert the position so it is in a 0-1 range on both axis.
+				var light_pos = relative_mouse_pos / viewport_container_node.rect_size
+				# Multiple by the viewport size so the position is within viewport space.
+				light_pos *= $GUI/ViewportContainer/Viewport.size
+				# Finally, set the position.
+				light2d_node.global_position = light_pos
+
+
+# Changes the Light2D node color based on input from the ColorPickerButton
+func _on_ColorPickerButton_color_changed(color):
+	light2d_node.color = color
